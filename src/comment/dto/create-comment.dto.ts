99a854895b1db9f31a56e00f115d9d1e28034e14
maxmaxph <maxmaxph@gmail.com>
2023-09-08 14:21:33 +0200

    6 mise à jour des DTO

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCommentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateCommentDto {
+  @ApiProperty()
+  user_id: number;
+
+  @ApiProperty()
+  store_id: number;
+
+  @ApiProperty()
+  note: number;
+}
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -3,20 +3,26 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
   @ApiProperty()
   name: string;
+
   @ApiProperty()
   firstname: string;
+
   @ApiProperty()
   pseudo: string;
+
   @ApiProperty()
   mail: string;
+
   @ApiProperty()
   phone: string | null;
+
   @ApiProperty()
   password: string;
-  @ApiProperty()
-  date_in: Date | null;
-  @ApiProperty()
-  date_out: Date | null;
-  @ApiProperty()
-  role_id: number;
+
+  //uniquement ce qu'on va recevoir du Front
+  // date_in: Date | null;
+  // @ApiProperty()
+  // date_out: Date | null;
+  // @ApiProperty()
+  // role_id: number;
 }
