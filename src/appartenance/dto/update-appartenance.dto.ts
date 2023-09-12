42a25b1667109c53916a801c9fe972a5a9eeaa13
alexandreg67 <alexandre.ag.67@gmail.com>
2023-09-12 14:24:19 +0200

    0.1.3 - suppression ressource appartenance + update comment

--- src/appartenance/dto/update-appartenance.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAppartenanceDto } from './create-appartenance.dto';
-
-export class UpdateAppartenanceDto extends PartialType(CreateAppartenanceDto) {}
