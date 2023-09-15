62247bf673d87cc1a7854804e3b9501c5b405050
kent108 <quentin.cutrim@gmail.com>
2023-09-15 14:45:32 +0200

    16 - Rajout de cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  //indique à NestJs qu'il va devoir checker les entrées avec class-validator
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('SimplonHub API')
@@ -14,6 +18,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  //permet d'utiliser cors pour lier front et back
+  app.enableCors();
 
   await app.listen(3000);
 }
