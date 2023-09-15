d35cc54b1f8d40f469cc5bc8c5ca3598a1b964db
maxmaxph <maxmaxph@gmail.com>
2023-09-15 09:54:16 +0200

    13 mise à jour du .env et du jwt strategy + creation .env.template

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -12,7 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private userRepository: Repository<User>,
   ) {
     super({
-      secretOrKey: 'heypeter',
+      secretOrKey: process.env.JWT_SECRET,
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
