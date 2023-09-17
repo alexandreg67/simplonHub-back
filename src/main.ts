6d800c2cce1082725fe2fc785e9ce0939748c1fc
alexandreg67 <alexandre.ag.67@gmail.com>
2023-09-17 16:00:26 +0200

    modification jwt-strategy

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   app.setGlobalPrefix(`api`);
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors(); // Utilisation de cors
+
   const config = new DocumentBuilder()
     .setTitle('Simplon Hub API')
     .setDescription('The Simplon Hub API description')
