afcfa17503bfac585261d1a7de8074002b63d696
kent108 <quentin.cutrim@gmail.com>
2023-09-19 16:48:44 +0200

    18 - MAJ auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException, Req, } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { LoginDto } from './dto/login.dto';
@@ -14,9 +14,24 @@ export class AuthController {
   }
 
   @Post('/login')
-  login(
-    @Body() loginDto: LoginDto,
-  ): Promise<{ accessToken: string }> {
+  login(@Body() loginDto: LoginDto): Promise<{ accessToken: string }> {
     return this.authService.login(loginDto);
+  }
+
+  @Post('/checktoken')
+  async validateUserToken(@Req() req: any): Promise<any> {
+    console.log('je suis dans post/auth/checktoken req : ', req);
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      throw new UnauthorizedException('Token non fourni');
+    }
+
+    const isValid = await this.authService.validateToken(token);
+    if (isValid) {
+      return { valid: true };
+    } else {
+      throw new UnauthorizedException('Token invalide');
+    }
   }
 }
